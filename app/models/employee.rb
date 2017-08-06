daab71eefd3bd0c8a1c89338c9f0ab68c95a605a
李江锋 <lijiangfeng@lijiangfengdeMacBook-Pro.local>
2017-08-06 10:03:20 +0800

    添加权限设置

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,5 @@
 class Employee < ApplicationRecord
+	has_and_belongs_to_many :roles
 
 	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
