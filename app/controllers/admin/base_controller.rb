6440832ea7add80fbd433d5a625636bee39ba3f5
jamst <jamstjamst@163.com>
2017-08-06 23:15:08 +0800

    合并权限管理&角色管理

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -6,9 +6,13 @@ class Admin::BaseController < ActionController::Base
     before_action :check_permission, :except => [:desboart]
 
     def check_permission
-       unless current_employee.roles.map{|role| role.permissions}.flatten.uniq.select{|per| per.controller_tag == "111#{controller_name}"}.size > 0
+       unless current_employee.roles.map{|role| role.permissions}.flatten.uniq.select{|per| per.controller_tag == "#{controller_name}"}.size > 0
          flash[:notice] = "你没有权限"
          redirect_to "/admin/employees/desboart"
        end
+       # unless current_employee.roles.map{|role| role.permissions}.flatten.uniq.select{|per| per.controller_tag == "#{controller_name}" && per.action_tag == "#{action_name}"}.size > 0
+       #   flash[:notice] = "你没有权限"
+       #   redirect_to "/admin/employees/desboart"
+       # end
     end
 end
