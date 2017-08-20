763a3f77c3a05a6382b95058c975e1ed624f4d68
李江锋 <lijiangfeng@lijiangfengdeMacBook-Pro.local>
2017-08-20 14:32:59 +0800

    添加员工倒入导出excell功能

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,4 +4,18 @@ class Employee < ApplicationRecord
 	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+    EMPLOYEE_COLUMNS = {
+    	name: 0,
+	    email: 1,
+	    mobile: 2
+    }     
+
+    def self.save_from_hash(hash)
+      employee = Employee.find_or_initialize_by(email: hash[:email])
+      employee.assign_attributes(name: hash[:name],
+                                 mobile: hash[:mobile],
+                                 password:111111)
+      employee.save
+    end     
+
 end
